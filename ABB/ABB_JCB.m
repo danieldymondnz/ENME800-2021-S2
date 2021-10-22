d3c4e373d46520b454f813e8ab888b1b34c9b95e
Daniel Dymond <daniel@danieldymond.nz>
2021-10-22 21:38:17 +1300

    Automation of Jacobian creation

--- ABB/ABB_JCB.m
+++ ABB/ABB_JCB.m
@@ -1,7 +1,24 @@
 %% Jacobian
 
 % Syms
-syms dT [6 1]; syms dW [1 6]; syms dV [1 6];
+syms dT [6 1]; syms dW [1 6]; syms dV [1 6]; syms C23; syms s23;
+
+%% Generate Simplifications
+% T_63
+T_43 = dhToTMatrix(DH, 4, 3);
+T_64 = dhToTMatrix(DH, 6, 4);
+T_63 = multiplyTransMatrix(T_43, T_64);
+
+% T_31
+T_31 = dhToTMatrix(DH, 3, 1);
+T_31 = simplify(T_31); T_31 = subs23Syms(T_31);
+
+% T_30
+T_10 = dhToTMatrix(DH, 1, 0);
+T_30 = multiplyTransMatrix(T_10, T_31);
+
+% T_60
+T_60 = multiplyTransMatrix(T_30, T_63);
 
 %% Get the Velocities relative to {0} by working up through {1} -> {6}
 
@@ -37,8 +54,10 @@ for i=0:5
 end
 
 % Determine Jacobian for Linear Velocity
-V_3 = V_ii{6}
+W_3 = W_ii{6}
+W_3 = collect(W_3, transpose(dT(:)))
 
+V_3 = V_ii{6}
 V_3 = collect(V_3, transpose(dT(:)))
 
 J_3 = {};
@@ -51,9 +70,19 @@ for i=1:3
     J_3{i, 4} = simplify(coefficients(4));
     J_3{i, 5} = simplify(coefficients(5));
     J_3{i, 6} = simplify(coefficients(6));
+    [coefficients,~] = equationsToMatrix(W_3(1,:),transpose(dT(:)))
+    J_3{3+i, 1} = simplify(coefficients(1));
+    J_3{3+i, 2} = simplify(coefficients(2));
+    J_3{3+i, 3} = simplify(coefficients(3));
+    J_3{3+i, 4} = simplify(coefficients(4));
+    J_3{3+i, 5} = simplify(coefficients(5));
+    J_3{3+i, 6} = simplify(coefficients(6));
 end
 
+R_60 = T_60(1:3, 1:3);
+J_0 = R_60 * J_3;
 
+% Find singularity
+determinant = det(J_0)
 
-%  test
-W_11 = [0; 0; dT1];
+J_0;
